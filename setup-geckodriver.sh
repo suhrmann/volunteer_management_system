65286a3da23f3216ac1589373ff3f633198aad6a
Simon Uhrmann <simon.uhrmann@th-deg.de>
2023-08-28 14:22:47 +0200

    Cleanup old Vagrantfile & setup-geckodriver.sh

--- setup-geckodriver.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Download the geckodriver tar
-wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-linux64.tar.gz
-# Unzip the binary
-tar -xzvf geckodriver-v0.26.0-linux64.tar.gz
-
-# Set geckodriver binary in your path
-sudo mv geckodriver /usr/local/bin
